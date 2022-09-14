f925b87dc87a823bb0b6f29c977cf0294d8c54b1
choozii <jiyechoo@gmail.com>
2022-09-14 21:51:56 +0900

    [algorithm] daily problem solving

--- Algorithm/문제 풀이/LeetCode/21_Merge_Two_Sorted_Lists.js
+++ Algorithm/문제 풀이/LeetCode/21_Merge_Two_Sorted_Lists.js
@@ -28,3 +28,39 @@ var mergeTwoLists = function(l1, l2) {
     mergedCurr.next = l1 || l2;
     return merged.next;
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+ var mergeTwoLists = function(list1, list2) {
+    let merged = new ListNode(null);
+    let traversal = merged;
+    
+    while(list1 !== null && list2 !== null){
+        if(list1.val < list2.val){
+            traversal.next = list1;
+            traversal = traversal.next;
+            list1 = list1.next;
+        }else{
+            traversal.next = list2;
+            traversal = traversal.next;
+            list2 = list2.next;
+        }
+    }
+    if(list1 !== null) traversal.next = list1;
+    if(list2 !== null) traversal.next = list2;
+    
+    return merged.next;
+};
+
+
