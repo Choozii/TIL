f39524fceb550139fe23e34758262d92730334e2
choozii <jiyechoo@gmail.com>
2022-09-15 16:18:25 +0900

    [algorithm] daily problem solving

--- Algorithm/문제 풀이/LeetCode/46. Permutations.js
+++ Algorithm/문제 풀이/LeetCode/46. Permutations.js
@@ -24,3 +24,28 @@
     dfs(0, []);
     return ans;
 };
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+ var permute = function(nums) {
+    const output = [];
+    const visited = Array.from({length : nums.length}, ()=>false);
+    
+  function recursive(l, res){
+      if(l === nums.length){
+          output.push(res);
+          return;
+      }
+      for(let i=0;i<nums.length;i++){
+          if(!visited[i]){
+              visited[i] = true;
+              recursive(l+1, [...res, nums[i]]);
+              visited[i] = false;
+          }
+      }
+  }
+    recursive(0, []);
+    return output;
+};
