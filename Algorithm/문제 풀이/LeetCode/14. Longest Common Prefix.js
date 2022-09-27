a0353636554c0332d8329e95018221b6a17902a1
choozii <jiyechoo@gmail.com>
2022-09-27 10:40:57 +0900

    [algorithm] daily problem

--- Algorithm/문제 풀이/LeetCode/14. Longest Common Prefix.js
+++ Algorithm/문제 풀이/LeetCode/14. Longest Common Prefix.js
@@ -1,32 +1,23 @@
-// 소문자 단어 배열에서 가장 긴 공통의 접두어를 찾기
-// 공통되는 string이 없다면 return ""
-
-// 예외 케이스 : input에 ""이 있다면 return ""
-
-
-var longestCommonPrefix = function(strs) {
-    // 각 단어를 돌면서
-    // 각 단어의 인덱스를 비교 -> 길이가 제일 짧은 단어만큼만 비교
-    // 비교는 map을 만들어서 하면 좋을 것 같음. 인덱스 : 값 꼴로
-    // 만약 같다면 common 변수에 저장
-    // 만약 strs 중에 ""인 단어가 있다면 return ""
-    let commonStr = "";
-    let map = {};
-        for(let i=0;i<strs.length;i++){
-            for(let j=0;j<strs[i].length;j++){
-                //각 단어의 인덱스를 돌 것
-                if(map[j]){
-                    if(map[j] !== strs[i][j]) console.log(commonStr);
-                }else{
-                    map[j] = strs[i][j];
-                }
-            }
-            console.log(map);
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+ var longestCommonPrefix = function(strs) {
+    let common = strs[0];
+    const getCommon = (str1, str2) => {
+        let p1 = 0, p2 = 0;
+        while(p1 < str1.length && p2 < str2.length){
+            if(str1[p1] === str2[p2]){
+                p1++;
+                p2++;
+            }else
+                break;
         }
-    // console.log(strs);
-    console.log(commonStr);
-
+        return str1.substr(0, p1);
+    }
+    
+    for(let i=1;i<strs.length;i++){
+        common = getCommon(common, strs[i]);
+    }
+    return common;
 };
-
-longestCommonPrefix(["flower","flow","flight"]);
-longestCommonPrefix(["dog","racecar","car"]);
